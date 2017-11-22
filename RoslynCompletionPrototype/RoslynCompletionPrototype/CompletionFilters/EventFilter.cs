492243e4b6e86c156a82d6e12d0d1d9cb3fe096d
Amadeusz Wieczorek <amwieczo@microsoft.com>
2017-11-22 14:31:33 -0800

    exports completion filters; updates the completion service

---
+++ RoslynCompletionPrototype/RoslynCompletionPrototype/CompletionFilters/EventFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense.Prototype.Definition;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoslynCompletionPrototype.CompletionFilters
+{
+    [Export(typeof(ICompletionFilter))]
+    [Name(nameof(EventFilter))]
+    [Order(After = nameof(MethodFilter))]
+    [ExportMetadata("ContentType", "CSharp")]
+    public class EventFilter : ICompletionFilter
+    {
+        public readonly ImmutableArray<string> _tags = ImmutableArray.Create("Event");
+        public const char _accessCharacter = 'x';
+        public const string _displayText = "Events";
+        private readonly ImageMoniker _knownMonikerTypePublic = new ImageMoniker(new Guid("{ae27a6b0-e345-4288-96df-5eaf394ee369}"), 1152);
+
+        public ImageMoniker Icon => _knownMonikerTypePublic;
+
+        string ICompletionFilter.DisplayText => _displayText;
+
+        ImmutableArray<string> ICompletionFilter.Tags => _tags;
+
+        char ICompletionFilter.AccessCharacter => _accessCharacter;
+    }
+}
--- RoslynCompletionPrototype/RoslynCompletionPrototype/RoslynCompletionService.cs
+++ RoslynCompletionPrototype/RoslynCompletionPrototype/RoslynCompletionService.cs
@@ -10,6 +10,7 @@ using Microsoft.CodeAnalysis.Text;
 using Microsoft.CodeAnalysis.Completion;
 using Microsoft.CodeAnalysis;
 using Prototype = Microsoft.VisualStudio.Language.Intellisense.Prototype.Definition;
+using System.Collections.Immutable;
 
 namespace RoslynCompletionPrototype
 {
@@ -19,19 +20,44 @@ namespace RoslynCompletionPrototype
     {
         private CompletionService CompletionService { get; set; }
 
-        async Task<Prototype.CompletionList> IAsyncCompletionService.UpdateCompletionListAsync(IEnumerable<Prototype.CompletionItem> originalList, ITextSnapshot snapshot, ITrackingSpan applicableSpan)
+        async Task<Prototype.CompletionList> IAsyncCompletionService.UpdateCompletionListAsync(IEnumerable<Prototype.CompletionItem> originalList, ITextSnapshot snapshot, ITrackingSpan applicableSpan, IEnumerable<ICompletionFilter> availableFilters)
         {
             var filterText = applicableSpan.GetText(snapshot);
             var filteredList = originalList.Where(n => n.FilterText.Contains(filterText)); // TODO: use pattern matcher
             var sortedList = filteredList.OrderBy(n => n.SortText);
-            var availableFilters = filteredList.SelectMany(n => n.Tags).Distinct().Select(n => new CompletionItemFilter(n, n, n[0]));
+            // Filtering (with filter buttons) should happen here rather than in the viewmodel, because viewmodel operates on UI thread
+            // and language service may want to do something interesting when there are no available items
+
+            // For now, just see which filters are available for all items
+            var allTags = originalList.SelectMany(n => n.Tags).Distinct();
+            var allFilters = availableFilters.Where(n => FilterMatchesTag(n, allTags));
+            // And which filters are available just for the visible items
+            var filteredTags = filteredList.SelectMany(n => n.Tags).Distinct();
+            var enabledFilters = availableFilters.Where(n => FilterMatchesTag(n, filteredTags));
+            // Create a filter dictionary where value indicates whether it's enabled
+            var filters = ImmutableDictionary.Create<ICompletionFilter, bool>();
+            filters = filters.AddRange(allFilters.Select(n => new KeyValuePair<ICompletionFilter, bool>(n, enabledFilters.Contains(n))));
 
             // TODO: optimize so we don't iterate three times
             bool suggestionMode = originalList.Any(n => n.IsSuggestion);
             bool softSelection = originalList.Any(n => n.SoftSelected);
             Prototype.CompletionItem suggestionModeItem = originalList.FirstOrDefault(n => n.IsSuggestion);
 
-            return new Prototype.CompletionList(sortedList, 0, softSelection, suggestionMode, suggestionModeItem, availableFilters);
+            return new Prototype.CompletionList(sortedList, 0, softSelection, suggestionMode, suggestionModeItem, filters);
         }
+
+        private static bool FilterMatchesTag(ICompletionFilter filter, IEnumerable<string> allTags)
+        {
+            foreach (var tag in allTags)
+            {
+                if (filter.Tags.Contains(tag))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // TODO: Bring ShouldBeFilteredOutOfCompletionList from Roslyn
     }
 }
